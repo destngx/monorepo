664287839c2547b00bf6cd28654952a63a25645d
destngx <chromevi123@gmail.com>
2024-11-13 23:31:33 +0700

    fix: chakra ui divider to separator

--- apps/portfolio-landpage/src/app/error.tsx
+++ apps/portfolio-landpage/src/app/error.tsx
@@ -1,13 +1,13 @@
 'use client';
 import NextLink from 'next/link';
-import { Box, Heading, Text, Container, Divider, Button } from '@chakra-ui/react';
+import { Box, Heading, Text, Container, Separator, Button } from '@chakra-ui/react';
 
 const NotFound = () => {
   return (
     <Container>
       <Heading as="h1">Not found</Heading>
       <Text>The page you&apos;re looking for was not found.</Text>
-      <Divider my={6} />
+      <Separator my={6} />
       <Box my={6} alignContent="center">
         <NextLink href="/">
           <Button colorScheme="teal">Return to home</Button>
