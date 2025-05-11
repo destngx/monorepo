bd70e5af71e0277dced5e694da3b8aedb89f673b
destngx <chromevi123@gmail.com>
2025-05-11 17:24:42 +0700

    fix(portfolio): archived all

--- apps/portfolio-landpage/src/app/error.tsx
+++ apps/portfolio-landpage/src/app/error.tsx
@@ -1,19 +1,10 @@
 'use client';
-import NextLink from 'next/link';
-import { Box, Heading, Text, Container, Separator, Button } from '@chakra-ui/react';
 
 const NotFound = () => {
   return (
-    <Container>
-      <Heading as="h1">Not found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
-      <Separator my={6} />
-      <Box my={6} alignContent="center">
-        <NextLink href="/">
-          <Button colorScheme="teal">Return to home</Button>
-        </NextLink>
-      </Box>
-    </Container>
+  <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-500 to-purple-500">
+      Error
+  </div>
   );
 };
 
--- apps/portfolio-landpage/src/app/page.tsx
+++ apps/portfolio-landpage/src/app/page.tsx
@@ -1,108 +1,16 @@
 'use client';
-import { BioSection, BioYear, Navbar, Paragraph, ThreeModel } from '@portfolio-landpage/components';
-import NextLink from 'next/link';
-import { IoLogoGithub, IoLogoYoutube } from 'react-icons/io5';
-import { FaCameraRetro, FaSpotify } from 'react-icons/fa';
+
 import React from 'react';
-import { motion } from 'framer-motion';
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual';
 }
 
-const projectList = [
-  { projectName: 'Photo Gallery', url: 'https://destnguyxn-photos.vercel.app/', year: 2023 },
-  { projectName: 'ProjectName', url: '', year: 2023 },
-  { projectName: 'ProjectName', url: '', year: 2023 },
-  { projectName: 'ProjectName', url: '', year: 2023 },
-];
+
 const Home = () => {
   return (
     <div className={' flex '}>
-      <Navbar />
-      <div className={'m-2 grow grid grid-cols-1 md:grid-cols-2 gap-6'}>
-        <div className={'mt-60 m-2 p-1 flex-col flex gap-6'}>
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            className="w-full font-thin backdrop-blur text-gray-50 bg-black/10 p-2 rounded shadow"
-          >
-            Hi! I&apos;m Nguyen Pham Quang Dinh, a full-stack developer based in Ho Chi Minh, Viet Nam
-          </motion.div>
-          <div className={'py-2'}>
-            <BioSection>
-              <BioYear>1999</BioYear>
-              Born in Lam Dong, Viet Nam.
-            </BioSection>
-            <BioSection>
-              <BioYear>2022</BioYear>
-              Graduated with a Bachelor’s degree in Software Engineering from the Faculty of Information Technology at
-              the University of Science, Ho Chi Minh City National University.
-            </BioSection>
-            <BioSection>
-              <BioYear>2023</BioYear>
-              Working at a top game company from Korea.
-            </BioSection>
-            <BioSection>
-              <BioYear>2024</BioYear>
-              Concurrently being a freelancer and learning new things
-            </BioSection>
-          </div>
-          <Paragraph>I ♥ Art, Drawing, Music , Playing Guitar, Photography, AI, and Machine Learning</Paragraph>
-          <div>
-            Find me on the web
-            <div className={'flex flex-col'}>
-              <NextLink
-                href="https://open.spotify.com/user/22uqfqyasviijy57kq2fnxv3a?si=591a37358eb34a77"
-                target="_blank"
-                className={'w-fit flex flex-row font-bold gap-2 align-middle items-center'}
-              >
-                <FaSpotify />
-                Spotify ↗
-              </NextLink>
-              <NextLink
-                className={'w-fit flex flex-row font-bold gap-2 align-middle items-center'}
-                href={'https://github.com/destnguyxn'}
-                target="_blank"
-              >
-                <IoLogoGithub />
-                Github ↗
-              </NextLink>
-              <NextLink
-                className={'w-fit flex flex-row font-bold gap-2 align-middle items-center'}
-                href={'https://www.youtube.com/'}
-                target="_blank"
-              >
-                <IoLogoYoutube />
-                Youtube ↗
-              </NextLink>
-              <NextLink
-                className={'w-fit flex flex-row font-bold gap-2 align-middle items-center'}
-                href="https://www.eyeem.com/u/destnguyxn"
-                target="_blank"
-              >
-                <FaCameraRetro />
-                Eyeem ↗
-              </NextLink>
-            </div>
-          </div>
-        </div>
-        <div className={'flex flex-col text-right gap-4'}>
-          <ThreeModel />
-          <div>Projects</div>
-          {projectList.map((project) => {
-            return (
-              <NextLink
-                key={Math.random()}
-                href={project.url}
-                className={'flex flex-row-reverse justify-items-start gap-4'}
-              >
-                <div className={'text-5xl'}>{project.projectName}</div>
-                <div className={'self-end'}>{project.year} /</div>
-              </NextLink>
-            );
-          })}
-        </div>
-      </div>
+      Hello
     </div>
   );
 };
